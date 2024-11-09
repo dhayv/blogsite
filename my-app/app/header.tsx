6d699679064c1ae7f01d00edeeec2ae91ed427bd
Hayvid <davidhyppolitedh@gmail.com>
2024-11-09 13:14:23 -0500

    added header

---
+++ my-app/app/header.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/file.svg"
+          alt="File icon"
+          width={16}
+          height={16}
+        />
+        Learn
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
+        Examples
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        Go to nextjs.org →
+      </a>
+    </header>
+  )
+}
+
+export default Header;
