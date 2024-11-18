e3be40db32b60a58c255fb6a33dbd9d0585ceafa
Hayvid <davidhyppolitedh@gmail.com>
2024-11-18 14:17:56 -0500

    update s3 bucket

--- terraform-config/modules/blog/s3.tf
+++ terraform-config/modules/blog/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = var.domain_name
   acl    = "private"
 
   control_object_ownership = true
@@ -9,7 +9,45 @@ module "s3_bucket" {
 
   versioning = {
     enabled = true
+  } 
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 
-  
+  error_document {
+    key = "error.html"
+  }
 }
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  acl = "private"
+}
+
+locals {
+    mime_types = {
+      ".html" = "text/html"
+      ".png"  = "image/png"
+      ".jpg"  = "image/jpeg"
+      ".gif"  = "image/gif"
+      ".css"  = "text/css"
+      ".js"   = "application/javascript"
+    }
+}
+
+resource "aws_s3_object" "build" {
+  for_each = fileset("../my-app/app/", "**")
+  bucket = aws_s3_bucket.www-my-aws-project-com.id
+  key = each.value
+  source = "../my-app/app/${each.value}"
+  etag = filemd5("../my-app/app/${each.value}")
+  acl    = "private"
+content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.key), null)
+}
+
