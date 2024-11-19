135baa0425f06211592551f009317f2ccd9e6a66
Hayvid <davidhyppolitedh@gmail.com>
2024-11-19 12:37:02 -0500

    update to select proper hosted zone

--- terraform-config/modules/blog/route53.tf
+++ terraform-config/modules/blog/route53.tf
@@ -3,13 +3,13 @@ resource "aws_cloudfront" "my_distribution" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = data.aws_route53_zone.selected.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
     name                   = aws_cloudfront_distribution.my_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.my_distribution.zone_id
+    zone_id                = aws_cloudfront_distribution.my_distribution.hosted_zone_id
     evaluate_target_health = true
   }
 }
