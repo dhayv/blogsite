5ce356cb4706f5fb35b0e8c67892493056bfa544
Hayvid <davidhyppolitedh@gmail.com>
2024-11-19 14:52:34 -0500

    updated file names usability

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -13,43 +13,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "domain_name" {
+  type = string
+  
+}
+
 module "s3_bucket" {
   source = "./modules/blog/s3.tf"
   bucket = var.domain_name
-  acl    = "private"
+  acl = "private"
+  versioning_enabled = true
 
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
+} 
 
-  versioning = {
-    enabled = true
-  } 
-
-    index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-  tags = {
-    Name = "My blog bucket"
-  }
+module "origin_access" {
+  source = "./modules/blog"
+  domain_name = var.domain_name
 }
 
-module "ACM" {
+
+module "acm" {
   source = "./modules/blog/acm.tf"
+  domain = var.domain_name
 }
 
-module "aws_cloudfront" {
+module "cloudfront" {
   source = "./modules/blog/cloudfront.tf"
+  domain = var.domain_name
 }
 
-module "aws_route53_zone" {
+module "route53" {
   source = "./modules/blog/route53.tf"
+  domain = var.domain_name
+  cloudfront_domain_name = module.cloudfront.cloudfront_domain_name
+  cloudfront_hosted_zone_id = module.cloudfront.cloul
+
 }
 
 module "s3_policy" {
   source = "./modules/blog/s3-policy.tf"
+  domain = var.domain_name
+  origin_access_control_arn = module.origin_access.origin_access_control_id
 }
