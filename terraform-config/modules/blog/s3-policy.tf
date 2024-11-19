8b71e7199847c4b42426ec001fc0622cd049fd01
Hayvid <davidhyppolitedh@gmail.com>
2024-11-19 12:37:20 -0500

    update to call proper bucket

--- terraform-config/modules/blog/s3-policy.tf
+++ terraform-config/modules/blog/s3-policy.tf
@@ -1,31 +1,11 @@
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.domain_name.arn}/*"]
+    resources = ["${aws_s3_bucket.blog}/*"]
 
     principals {
       type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.example.iam_arn]
+      identifiers = [aws_cloudfront_origin_access_identity.default.iam_arn]
     }
   }
-}
-
-resource "aws_s3_bucket_policy" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.s3_bucket.arn,
-          "${aws_s3_bucket.s3_bucket.arn}/*",
-        ]
-      },
-    ]
-  })
 }
