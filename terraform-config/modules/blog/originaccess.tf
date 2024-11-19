4e95b4f7f35a83cf93de2d7c303c1e9b2bfe855d
Hayvid <davidhyppolitedh@gmail.com>
2024-11-19 12:36:19 -0500

    update to use origin access control

--- terraform-config/modules/blog/originaccess.tf
+++ terraform-config/modules/blog/originaccess.tf
@@ -1,3 +1,7 @@
-resource "aws_cloudfront_origin_access_identity" "blog_oai" {
-    comment = "${var.domain_name}"
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "${var.domain_name} OAC"
+  description                       = "Managed by Terraform"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
