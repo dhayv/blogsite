70cbb97cd7b761a298a9dbd4780751030832930d
Hayvid <davidhyppolitedh@gmail.com>
2024-11-20 11:00:52 -0500

    removed redundant code

--- terraform-config/modules/blog/cloudfront/main.tf
+++ terraform-config/modules/blog/cloudfront/main.tf
@@ -1,16 +1,3 @@
-resource "aws_s3_bucket" "blog" {
-  bucket = var.domain_name
-
-  tags = {
-    Name = "My blog bucket"
-  }
-}
-
-resource "aws_s3_bucket_acl" "blog_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-}
-
 locals {
   s3_origin_id = "myS3Origin"
 }
@@ -24,82 +11,34 @@ resource "aws_cloudfront_distribution" "my_distribution" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Some comment"
+  comment             = "Cloudfront distribution fro ${var.domain_name}"
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
 
-  aliases = ["www.${var.domain_name}", var.domain_name]
+  aliases = [var.domain_name, "www.${var.domain_name}"]
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
-      headers      = ["Origin"]
 
       cookies {
         forward = "none"
       }
     }
 
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
     viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
 
-  price_class = "PriceClass_200"
+  
+
+  price_class = "PriceClass_100"
 
   restrictions {
     geo_restriction {
@@ -108,9 +47,6 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     }
   }
 
-  tags = {
-    Environment = "production"
-  }
 
   viewer_certificate {
     acm_certificate_arn = var.acm_certificate_arn
@@ -119,11 +55,3 @@ resource "aws_cloudfront_distribution" "my_distribution" {
   }
 }
 
-// connect to route53 
-output "distribution_domain_name" {
-  value = aws_cloudfront_distribution.my_distribution.domain_name
-}
-
-output "distribution_hosted_zone_id" {
-  value = aws_cloudfront_distribution.my_distribution.hosted_zone_id
-}
