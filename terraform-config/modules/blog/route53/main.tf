588fcf75045a2e1b647a5086cd7af93dbf769bb3
Hayvid <davidhyppolitedh@gmail.com>
2024-11-20 11:02:54 -0500

    update ,to  both acm aliases connect to cloudfront distribution

--- terraform-config/modules/blog/route53/main.tf
+++ terraform-config/modules/blog/route53/main.tf
@@ -1,10 +1,19 @@
-resource "aws_cloudfront" "my_distribution" {
-  name               = "blog-terraform"
-}
-
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "aliad" {
   zone_id = var.zone_id
   name    = var.domain_name
+  type    = "A"
+
+  alias {
+    // received from cloudfront output
+    name                   = var.cloudfront_distribution_domain_name
+    zone_id                = var.cloudfront_distribution_hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "alias_www" {
+  zone_id = var.zone_id
+  name    = "www.${var.domain_name}"
   type    = "A"
 
   alias {
