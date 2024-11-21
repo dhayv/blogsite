d0925bca08ed5c8e522667e8a2c010ac9e819308
Hayvid <davidhyppolitedh@gmail.com>
2024-11-21 17:39:05 -0500

    proper protocol version referenced

--- terraform-config/modules/blog/cloudfront/main.tf
+++ terraform-config/modules/blog/cloudfront/main.tf
@@ -51,7 +51,7 @@ resource "aws_cloudfront_distribution" "my_distribution" {
   viewer_certificate {
     acm_certificate_arn = var.acm_certificate_arn
     ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLS1.2_2021"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
