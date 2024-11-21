300a598a6d37d533fdcd6e73333a8f59ec92c9ae
Hayvid <davidhyppolitedh@gmail.com>
2024-11-21 17:36:55 -0500

    removed ununsed code

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./header";
 import RecentPost from "./recentpost";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "David Hyppolite's Blog",
@@ -26,10 +14,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
           <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-4 pb-20 gap-10 sm:p-16 font-[family-name:var(--font-geist-sans)]">
           <Header/>
           <main>{children}</main>
@@ -38,8 +22,5 @@ export default function RootLayout({
           <footer className="flex gap-6 flex-wrap items-center justify-center"> 
             © {new Date().getFullYear()} . All rights reserved.</footer>
           </div>
-         
-        </body>
-    </html>
   );
 }
