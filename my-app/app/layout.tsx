a7bf82c69b825d7f31eb052b646d03c9eda25a12
Hayvid <davidhyppolitedh@gmail.com>
2024-11-22 10:00:35 -0500

    update to restore html and body code

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./header";
 import RecentPost from "./recentpost";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "David Hyppolite's Blog",
@@ -14,13 +26,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-          <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-4 pb-20 gap-10 sm:p-16 font-[family-name:var(--font-geist-sans)]">
+    <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div className="grid grid-rows-[auto_1fr_auto_auto] min-h-screen p-4 pb-20 gap-10 sm:p-16 font-[family-name:var(--font-geist-sans)]">
           <Header/>
           <main>{children}</main>
-          <RecentPost/>
           
           <footer className="flex gap-6 flex-wrap items-center justify-center"> 
             © {new Date().getFullYear()} . All rights reserved.</footer>
           </div>
+         
+        </body>
+    </html>
   );
 }
