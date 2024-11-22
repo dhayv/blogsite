b7b60ca9653ef922b8c1f1564547ec2f22a9e93e
Hayvid <davidhyppolitedh@gmail.com>
2024-11-22 10:01:09 -0500

    update typscirpt

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,16 @@
-const Home = (): JSX.Element => {
+import RecentPost from './recentpost';
+
+const Home: React.FC = () => {
 
   return (
     <div className=" max-w-xl mx-auto w-full">
       
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <h1>Welcome to my Blog</h1>
-        <p>My Name is David Hyppolite a self-taught Cloud Engineer. On this blog I will sharing and Documenting my Journey and thoughts and technologies I am using.</p>
+        
+        <p>My Name is David Hyppolite a self-taught Cloud Engineer. On this blog I will sharing and Documenting my Journey and thoughts and technologies I am using. This entire blog's architecture is built using Terraform, AWS
+        (S3, CloudFront, Route53, ACM), GitHub Actions for CI/CD, and Next.js.</p>
+        <RecentPost />
       </main>
     </div>
   )
