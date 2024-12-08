cefeacace0d21adbfa2d99e149c3be1ac0601a32
Hayvid <davidhyppolitedh@gmail.com>
2024-12-08 10:24:10 -0500

    remove code

--- terraform-config/modules/blog/s3/main.tf
+++ terraform-config/modules/blog/s3/main.tf
@@ -49,17 +49,3 @@ data "aws_iam_policy_document" "bucket_policy" {
     }
   }
 }
-
-
-resource "aws_s3_bucket_website_configuration" "blog" {
-  bucket = aws_s3_bucket.blog.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-
-}
