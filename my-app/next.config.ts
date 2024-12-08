406a2c5d897772b199c1e682bee7ba395560c9b5
Hayvid <davidhyppolitedh@gmail.com>
2024-12-08 15:03:58 -0500

    add explicit paths for blog

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: async function() {
+    return {
+      '/': { page: '/' },
+      '/blog': { page: '/blog' },
+      // Add explicit paths for your blog posts
+      '/blog/terraform-autoscaling-group': { page: '/blog/[slug]', query: { slug: 'terraform-autoscaling-group' } },
+      '/blog/building-a-blog-with-aws-terraform': { page: '/blog/[slug]', query: { slug: 'building-a-blog-with-aws-terraform' } }
+    }
+  }
 };
 
 export default nextConfig;
