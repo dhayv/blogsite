1fc36ec7cee5e3af1bdf88ab21895af1b37963b9
Hayvid <davidhyppolitedh@gmail.com>
2024-12-08 15:18:15 -0500

    removed lines

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -5,15 +5,6 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-      '/blog': { page: '/blog' },
-      // Add explicit paths for your blog posts
-      '/blog/terraform-autoscaling-group': { page: '/blog/[slug]', query: { slug: 'terraform-autoscaling-group' } },
-      '/blog/building-a-blog-with-aws-terraform': { page: '/blog/[slug]', query: { slug: 'building-a-blog-with-aws-terraform' } }
-    }
   }
 };
 
