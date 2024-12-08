05e03c531c427765c16e3aa57b1aa3cbf1189f63
Hayvid <davidhyppolitedh@gmail.com>
2024-12-08 11:22:49 -0500

    added variable values

--- terraform-config/modules/blog/cloudfront/variables.tf
+++ terraform-config/modules/blog/cloudfront/variables.tf
@@ -16,13 +16,15 @@ variable "bucket_name" {
 
 variable "s3_caching_policy" {
   type = string
+  default = "658327ea-f89d-4fab-a63d-7e88639e58f6"
 }
 
 variable "s3_request_id" {
   type = string
+  default = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"
 }
 
-
 variable "header_policy" {
-  
+  type = string
+  default = "5cc3b908-e619-4b99-88e5-2cf7f45965bd"
 }
