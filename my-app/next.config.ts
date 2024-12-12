636c344ae33a9677f127f6675e0cb98c3dc7a92a
Hayvid <davidhyppolitedh@gmail.com>
2024-12-12 15:57:15 -0500

    update

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,10 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: false,
-  generateStaticParams: true,
   images: {
     unoptimized: true, 
   },
-};
+}; 
 
 export default nextConfig;
