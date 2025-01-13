e388970998ed0643ec7a5fb1a78af78e78a9893a
Hayvid <davidhyppolitedh@gmail.com>
2025-01-13 04:59:15 -0500

    update homepage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
         <h1 className='text-3xl font-extrabold'>Welcome to My Corner of the Internet</h1>
         
         <p>I'm David Hyppolite a Cloud/DevOps Engineer specializing in AWS, Terraform, Kubernetes, Linux, Docker, CI/CD, automation. This blog isn't just another tech space - it's where determination meets cloud innovation.</p>
-        <p>This blog is orchestrated through Terraform, powered by AWS (S3, CloudFront, Route53, ACM), automated using GitHub Actions, and brought to life with Next.js. Every component represents another challenge conquered, another level mastered.</p>
+        <p>This blog is orchestrated through <strong>Terraform</strong>, powered by AWS (S3, CloudFront, Route53, ACM), automated and deployed with <strong>GitHub Actions</strong>, and brought to life with Next.js. Every component represents another challenge conquered, another level mastered.</p>
         <p>Here, I'll document my journey, share my victories (and battles) with cloud technologies, and demonstrate how pure determination can transform ambitious goals into reality.</p>
        { /**/}
        <RecentPost />
